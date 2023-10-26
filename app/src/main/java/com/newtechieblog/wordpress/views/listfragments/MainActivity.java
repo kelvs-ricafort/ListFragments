04a3f701c27c7ecce78c6752ae94665b7df802c9
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-26 16:42:23 +0800

    ListFragments

--- app/src/main/java/com/newtechieblog/wordpress/views/listfragments/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/listfragments/MainActivity.java
@@ -1,6 +1,8 @@
 package com.newtechieblog.wordpress.views.listfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragMan = getSupportFragmentManager();
+        FragmentTransaction fragTrans = fragMan.beginTransaction();
+
+        MyListFragment listFragment = new MyListFragment();
+        fragTrans.add(R.id.frame, listFragment);
+
+        fragTrans.commit();
+
     }
 }
